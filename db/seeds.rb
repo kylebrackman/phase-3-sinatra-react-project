252f9227962b13a524921c06f21115731ed140ab
Kyle Brackman <kyle.a.brackman@gmail.com>
2023-01-15 15:17:42 -0700

    seeded user table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,10 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+User.create(username: "Kyle")
+User.create(username: "Lauren")
+User.create(username: "Greg")
+User.create(username: "Hayden")
+User.create(username: "Anne")
 
 puts "✅ Done seeding!"
